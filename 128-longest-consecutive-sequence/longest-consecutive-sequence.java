d4a9a536b7fae430ea7e62eb77ca618d85c75530
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2025-04-20 11:44:52 +0530

    Time: 55 ms (10.86%) | Memory: 60.2 MB (70.09%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,28 +1,32 @@
-class Solution {
-    public int longestConsecutive(int[] nums) {
-        int LongestLength = 0;
-        Map<Integer, Boolean> exploredMap = new HashMap<>();
-        for(int num : nums){
-            exploredMap.put(num, Boolean.FALSE);
-        }
-        for(int num : nums){
-            int currentLength = 1;
+class Solution{
+    public int longestConsecutive(int[] nums){
 
-            int nextNum = num + 1;
-            while(exploredMap.containsKey(nextNum) && exploredMap.get(nextNum) == false){
-                currentLength++;
-                exploredMap.put(nextNum, Boolean.TRUE);
-                nextNum++;
-            }
-            int prevNum = num -1;
-            while(exploredMap.containsKey(prevNum) && !exploredMap.get(prevNum)){
-                currentLength++;
-                exploredMap.put(prevNum,Boolean.TRUE);
-                prevNum--;
-            }
-            LongestLength = Math.max(LongestLength,currentLength);
+    int LongestLength = 0;
+        Map<Integer,Boolean> travelled = new HashMap<>();
+        for (int num : nums){
+            travelled.put(num, Boolean.FALSE);
         }
-        return LongestLength;
-        
+        for( int num : nums){
+            int currentLength = 1;
+            int nextNum = num+1;
+            while(travelled.containsKey(nextNum) &&(travelled.get(nextNum) == false)){
+
+            currentLength++;
+            travelled.put(nextNum, Boolean.TRUE);
+            nextNum++;
+            
+        }
+
+        //Check previous Element
+        int prevNum = num -1;
+        while(travelled.containsKey(prevNum) && (travelled.get(prevNum) == false)){
+        currentLength++;
+        travelled.put(prevNum, Boolean.TRUE);
+        prevNum --;
+
     }
+    LongestLength = Math.max(LongestLength,currentLength);
+}
+return LongestLength;
+}
 }
