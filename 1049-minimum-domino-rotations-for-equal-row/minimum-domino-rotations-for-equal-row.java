2876824d5e9b87d0a00eb27fe37b3aada76fb9a6
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2025-05-03 19:57:45 +0530

    Time: 2 ms (100.00%) | Memory: 50.5 MB (53.07%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.java
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.java
@@ -1,32 +1,24 @@
 class Solution {
-    public int minDominoRotations(int[] tops, int[] bottoms) {
-        // Try to make all values equal to tops[0]
-        int result = check(tops[0], tops, bottoms);
-        // If it's not possible with tops[0], try with bottoms[0]
-        if (result != -1) return result;
-        return check(bottoms[0], tops, bottoms);
+    private int tryRotationMatching(int number, int[] tops, int[] bottoms) {
+        int count = 0;
+        for (int i = 0; i < tops.length; ++i) {
+            if (tops[i] == number) continue;
+            else if (bottoms[i] == number) count++;
+            else return Integer.MAX_VALUE;
+        }
+        return count;
     }
 
-    private int check(int target, int[] tops, int[] bottoms) {
-        int topRotations = 0;     // Rotations to make all tops == target
-        int bottomRotations = 0;  // Rotations to make all bottoms == target
-
-        for (int i = 0; i < tops.length; i++) {
-            // If neither top nor bottom has the target value at i, not possible
-            if (tops[i] != target && bottoms[i] != target) {
-                return -1;
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        int minRotations = Integer.MAX_VALUE;
+        for (int number = 1; number <= 6; number++) {
+            int countTops = tryRotationMatching(number, tops, bottoms);
+            int countBottoms = Integer.MAX_VALUE;
+            if (countTops != Integer.MAX_VALUE) {
+                countBottoms = tryRotationMatching(number, bottoms, tops);
             }
-            // If top doesn't have the target, we need to rotate
-            else if (tops[i] != target) {
-                topRotations++;
-            }
-            // If bottom doesn't have the target, we need to rotate
-            else if (bottoms[i] != target) {
-                bottomRotations++;
-            }
+            minRotations = Math.min(minRotations, Math.min(countTops, countBottoms));
         }
-
-        // Return the minimum rotations needed to make all elements equal to target
-        return Math.min(topRotations, bottomRotations);
+        return minRotations == Integer.MAX_VALUE ? -1 : minRotations;
     }
 }
