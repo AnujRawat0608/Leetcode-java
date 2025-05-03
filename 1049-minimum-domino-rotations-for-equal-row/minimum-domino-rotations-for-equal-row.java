bc9b29ac8cac25d4b40ac52e42af7a9e0dc05c8b
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2025-05-03 15:32:53 +0530

    Time: 3 ms (95.73%) | Memory: 50.5 MB (53.07%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.java
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.java
@@ -1,24 +1,32 @@
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
-        int result = check(tops[0], tops, bottoms); // Try making tops[0] the target
+        // Try to make all values equal to tops[0]
+        int result = check(tops[0], tops, bottoms);
+        // If it's not possible with tops[0], try with bottoms[0]
         if (result != -1) return result;
-        return check(bottoms[0], tops, bottoms); // If failed, try with bottoms[0]
+        return check(bottoms[0], tops, bottoms);
     }
 
     private int check(int target, int[] tops, int[] bottoms) {
-        int rotateTop = 0;
-        int rotateBottom = 0;
+        int topRotations = 0;     // Rotations to make all tops == target
+        int bottomRotations = 0;  // Rotations to make all bottoms == target
 
         for (int i = 0; i < tops.length; i++) {
+            // If neither top nor bottom has the target value at i, not possible
             if (tops[i] != target && bottoms[i] != target) {
-                return -1; // Not possible
-            } else if (tops[i] != target) {
-                rotateTop++; // Need to rotate top to make it equal to target
-            } else if (bottoms[i] != target) {
-                rotateBottom++; // Need to rotate bottom to make it equal to target
+                return -1;
+            }
+            // If top doesn't have the target, we need to rotate
+            else if (tops[i] != target) {
+                topRotations++;
+            }
+            // If bottom doesn't have the target, we need to rotate
+            else if (bottoms[i] != target) {
+                bottomRotations++;
             }
         }
 
-        return Math.min(rotateTop, rotateBottom);
+        // Return the minimum rotations needed to make all elements equal to target
+        return Math.min(topRotations, bottomRotations);
     }
 }
