b991c02befca208993b547de79c0ac35dc496a1e
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2025-06-15 19:16:57 +0530

    Time: 4 ms (18.23%) | Memory: 40.9 MB (34.81%) - LeetSync

--- 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.java
+++ 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.java
@@ -1,19 +1,20 @@
 class Solution {
     public int minMaxDifference(int num) {
-        String s = String.valueOf(num);
-        int maxVal = Integer.MIN_VALUE;
-        int minVal = Integer.MAX_VALUE;
+      String str = Integer.toString(num);
 
-        // Try replacing every digit d1 with every digit d2 (0-9)
-        for (char d1 = '0'; d1 <= '9'; d1++) {
-            for (char d2 = '0'; d2 <= '9'; d2++) {
-                String replaced = s.replace(d1, d2);
-                int val = Integer.parseInt(replaced);
-                maxVal = Math.max(maxVal, val);
-                minVal = Math.min(minVal, val);
-            }
+      int MaxNum = Integer.MIN_VALUE;
+      int MinNum = Integer.MAX_VALUE;
+
+      for(char i='0'; i<='9'; i++){
+        for(char j='0'; j <= '9'; j++){
+
+            String replaced = str.replace(i,j);
+            int val = Integer.parseInt(replaced);
+
+            MaxNum = Math.max(MaxNum, val);
+            MinNum = Math.min(MinNum, val);
         }
-
-        return maxVal - minVal;
+      }
+      return MaxNum - MinNum;
     }
 }
