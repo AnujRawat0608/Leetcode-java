4d51958c18da4759bc31b9a1f8012b0f151cc315
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-01-27 02:19:25 +0530

    Time: 27 ms (59.68%) | Memory: 100.8 MB (19.71%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
@@ -1,41 +1,45 @@
 class RandomizedSet {
-   
-       List<Integer> list = new ArrayList<>();
-        Random random = new Random();
-      
 
-    
-    public boolean insert(int val)//returns true if the item was not present else false//
-    {
-        if(list.contains(val)){
+    private ArrayList<Integer> list;
+    private HashMap<Integer, Integer> map;
+    private Random rand;
+
+    public RandomizedSet() {
+        list = new ArrayList<>();
+        map = new HashMap<>();
+        rand = new Random();
+    }
+
+    public boolean insert(int val) {
+        if (map.containsKey(val)) {
             return false;
         }
         list.add(val);
+        map.put(val, list.size() - 1);
         return true;
-        
     }
-    
-    public boolean remove(int val)//retrun true if item was present else false
-    {
-        if(!list.contains(val)){
+
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) {
             return false;
         }
-        list.remove((Integer)val);
-        return true;
-        
 
+        int index = map.get(val);
+        int lastElement = list.get(list.size() - 1);
+
+        // Move last element to the index of val
+        list.set(index, lastElement);
+        map.put(lastElement, index);
+
+        // Remove last element
+        list.remove(list.size() - 1);
+        map.remove(val);
+
+        return true;
     }
-    
+
     public int getRandom() {
-        int randomIndex = random.nextInt(list.size());
+        int randomIndex = rand.nextInt(list.size());
         return list.get(randomIndex);
     }
 }
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet obj = new RandomizedSet();
- * boolean param_1 = obj.insert(val);
- * boolean param_2 = obj.remove(val);
- * int param_3 = obj.getRandom();
- */
