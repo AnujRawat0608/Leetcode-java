6a7eb3ecbb6d47c0a9926ae93ebd159d403ac44f
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-04-18 19:44:02 +0530

    Time: 2 ms (95.11%) | Memory: 65 MB (82.62%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,29 +1,24 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int zeroCount = 0; 
-        int productWithouZero = 1;
-        for(int num : nums){
-            if(num == 0)
-            zeroCount++;
-            else
-            productWithouZero *= num ;
+        int n =nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        left[0] = 1;
+        right[n-1] = 1;
+
+        for(int i=1; i<n; i++){
+            left[i] = left[i-1] * nums[i-1];
+        }
+        for(int i=n-2; i>=0; i--){
+            right[i] = right[i+1] * nums[i+1];
+        }
+        int[] result = new int[n];
+        for(int i=0; i<n; i++){
+            result[i] = left[i] * right[i];
         }
 
-        int[] result = new int[nums.length];
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] != 0){
-                if(zeroCount > 0)
-                result[i] = 0;
-                else
-                result[i] = productWithouZero / nums[i];
-            }else{
-                if(zeroCount > 1)
-                result[i] = 0;
-                else
-                result[i] = productWithouZero;
-            }
-        }
+
         return result;
-
     }
 }
