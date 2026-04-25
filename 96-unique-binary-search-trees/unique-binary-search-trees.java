69e587d31759016338ad118a9e1f30f440f9ea4d
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-04-25 22:38:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (5.01%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,24 +1,18 @@
 class Solution {
-    int[] memo;
-    
+    int[] memo = new int[20];
+
     public int numTrees(int n) {
-        memo = new int[n+1];
-        return solve(n);
-    }
-    private int solve(int n){
-
-        if(n==0 || n ==1)return 1;
-
-        if(memo[n] != 0) return memo[n];
-
         int count = 0;
-         for(int root = 1; root<=n; root++){
-            int left = solve(root-1);
-            int right = solve(n- root);
-            count += left * right; 
-         }
-         memo[n] = count;
-         return memo[n];
-        
+
+
+        if(n == 0 || n==1)return 1;
+        if(memo[n] != 0)return memo[n];
+        for(int root = 1; root <= n; root++){
+            int left = numTrees(root-1);
+            int right = numTrees(n - root);
+            count += left * right;
+        }
+        memo[n] = count;
+        return memo[n];
     }
 }
