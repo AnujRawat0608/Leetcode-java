1be92803fbe1b6ab4308e10c8b43aa62a2c1e776
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-05-12 13:49:20 +0530

    Time: 1 ms (100.00%) | Memory: 46.6 MB (37.71%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -11,37 +11,48 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-        ListNode result = new ListNode(0);
+        ListNode result = new ListNode();
         ListNode ptr = result;
 
         int carry = 0;
-        
 
         while(l1 != null || l2 != null){
             int sum = carry;
 
-        if(l1 != null){
-            sum += l1.val;
-            l1= l1.next;
+            if(l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if(l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            sum = sum % 10;
+            ptr.next = new ListNode(sum);
+            ptr = ptr.next;
         }
 
-        if(l2 != null){
-            sum += l2.val;
-            l2=l2.next;
-        }
+        if( carry == 1)ptr.next =new ListNode(1);
 
-        carry = sum / 10;
-        sum = sum %10;
-        ptr.next =new ListNode(sum);
-        ptr = ptr.next;
-    }
-    if(carry == 1) ptr.next = new ListNode(1);
-    return result.next;
-
-
-
-        
-        
+        return result.next;
         
     }
 }
+
+
+/*
+Dry run 
+l1 = 2,4,3 
+l2 = 5,6,4
+
+result [0]
+
+
+while(l1 != null || l2 != null)
+l1 + l2 = whatever ans we get ->we store it in sum variable 
+int carry
+
+*/
