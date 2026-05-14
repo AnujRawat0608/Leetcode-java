7731e12252c4c74bbe28cd2bb20a17f4bf30ca74
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-05-14 10:01:46 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (91.62%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.java
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -1,29 +1,30 @@
 class Solution {
     public boolean isGood(int[] nums) {
+        int n = 0;
         
-        int n =0 ;
-
+        // find the max element
         for(int num : nums){
-            n = Math.max(n, num);
+            n = Math.max(n,num);
         }
-
+        //length must be n+1
         if(nums.length != n+1){
             return false;
         }
-
+        //count frequency 
         int[] freq = new int[n+1];
         for(int num : nums){
             if(num > n){
                 return false;
+                }
+                freq[num]++;
             }
 
-            freq[num]++;
-        }
-        for(int i=1; i<n; i++){
-            if(freq[i] != 1){
-                return false;
+            for(int i =1; i< n; i++){
+                if(freq[i] != 1){
+                    return false;
+                }
             }
-        }
-        return freq[n] == 2;
+            return freq[n] ==2;
+        
     }
 }
