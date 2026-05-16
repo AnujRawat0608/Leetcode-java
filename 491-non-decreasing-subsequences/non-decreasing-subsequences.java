cea0ac26719b3deaa541476f850138cf568923eb
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-05-16 21:03:00 +0530

    Time: 6 ms (95.08%) | Memory: 52.8 MB (75.50%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.java
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.java
@@ -1,44 +1,34 @@
 class Solution {
+    int n ;
 
-    int n;
-    public void backtrack(int[] nums, int idx, List<Integer> curr, List<List<Integer>> result){
+    public void backtrack(int nums[], int idx,List<Integer> curr,List<List<Integer>> result ){
 
-        //If subsequence size > 1 , add to result
         if(curr.size() > 1){
             result.add(new ArrayList<>(curr));
         }
+            HashSet<Integer> set = new HashSet<>();
 
-        //To avoid duplicates at the sam recurssion level
-        HashSet<Integer> set = new HashSet<>();
 
-        for(int i = idx; i <n ; i++){
-            //check:
-            //1.Current number should maintain the non decresing order
-            //2.Avoid duplicates in same level
-
+        for(int i=idx; i<n; i++){
             if((curr.isEmpty() || nums[i] >= curr.get(curr.size() -1)) && !set.contains(nums[i])){
-                //choose
-                curr.add(nums[i]);
-
-                //Explore
-                backtrack(nums, i+1, curr,result);
-
-                //Backtrack
-                curr.remove(curr.size() -1);
-
-                //Mark number as used at this level
-                set.add(nums[i]);
-            }
+            curr.add(nums[i]);
+            backtrack(nums,i+1,curr,result);
+            curr.remove(curr.size()-1);
+            set.add(nums[i]);
         }
-
     }
+    }
+
+
+
 
     public List<List<Integer>> findSubsequences(int[] nums) {
 
         n = nums.length;
+
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> curr = new ArrayList<>();
-        backtrack(nums,0, curr,result);
+        backtrack(nums,0,curr,result);
         return result;
         
     }
