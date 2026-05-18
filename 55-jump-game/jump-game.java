07774cb92c5e7435f81ed535f36578e81c2983ac
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-05-18 16:55:16 +0530

    Time: 354 ms (5.96%) | Memory: 48.2 MB (8.65%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,28 @@
+class Solution {
+int[] dp;
+public boolean solve(int[] nums, int n, int idx){
+    if(idx >= n-1){
+        return true;
+    }
+    if(dp[idx] != -1){
+        return dp[idx] == 1;
+    }
+    for(int i=1; i<= nums[idx]; i++){
+        if(solve(nums, n, idx+i)){
+            dp[idx] =1;
+            return true;
+        }
+    }
+    dp[idx] = 0;
+    return false;
+}
+
+    public boolean canJump(int[] nums) {
+
+        int n = nums.length;
+        dp = new int[n];
+        Arrays.fill(dp,-1);
+        return solve(nums,n,0);
+        
+    }
+}
