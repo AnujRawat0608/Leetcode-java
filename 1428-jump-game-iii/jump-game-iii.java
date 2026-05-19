da5537d13fa571621444258132e6dbbb18850341
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-05-19 14:46:56 +0530

    Time: 8 ms (46.04%) | Memory: 48.2 MB (95.78%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.java
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -1,22 +1,28 @@
 class Solution {
-    int n ;
-    public boolean solve(int[]arr, int i){
-        if(i < 0 || i >= n || arr[i] < 0){
-            return false;
-        }
-        if(arr[i] == 0){
-            return true;
-        }
-        int val = arr[i];
-        arr[i]  = -1;
-        boolean left = solve(arr,i+ val);
-        boolean right = solve(arr, i-val);
-
-        return left || right;
-    }
     public boolean canReach(int[] arr, int start) {
-       n = arr.length;
-        return solve(arr,start);
-        
+        int n = arr.length;
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[n];
+
+        queue.add(start);
+        visited[start] = true;
+            while(!queue.isEmpty()){
+                int i = queue.poll();
+                if(arr[i] == 0)return true;
+
+                int left = i - arr[i];
+                int right= i + arr[i];
+
+                if(left >= 0 && !visited[left]){
+                    visited[left] = true;
+                    queue.add(left);
+                }
+                if(right < n && !visited[right]){
+                    visited[right] = true;
+                    queue.add(right);
+                }
+            }
+            return false;
+         
     }
 }
