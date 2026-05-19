f64e9689e15675ad106810f6d2f9e3b30d3590b0
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-05-20 02:54:10 +0530

    Time: 2 ms (87.94%) | Memory: 48.1 MB (8.65%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -7,7 +7,7 @@ class Solution {
                 return false;
             }
             maxReach = Math.max(maxReach, i+nums[i]);
-            
+            if(maxReach > nums.length-1)return true;   
         }
         return true;
         
