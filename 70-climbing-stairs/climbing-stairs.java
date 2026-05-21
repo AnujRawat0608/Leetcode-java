8eee53a1b0057c2b03fd5b694c3b2a41b17c0327
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-05-21 13:28:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (34.80%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,19 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n == 1) return 1;
-        
-        // create a table to store answers
-        int[] dp = new int[n + 1];
-        
-        // fill base cases
-        dp[1] = 1;
-        dp[2] = 2;
-        
-        // fill rest bottom-up
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        if( n == 0 || n == 1 || n == 2){
+            return n;
         }
+        int[] arr = new int[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
+
+        for(int i=3; i<=n; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        return arr[n];
         
-        return dp[n];
     }
 }
