6421772ab88bed901e09b6f5c62ded0c958ade6d
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-05-22 10:42:10 +0530

    Time: 24 ms (21.18%) | Memory: 47.2 MB (11.36%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.java
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.java
@@ -1,24 +1,28 @@
 class Solution {
-    int total;
-    private void findSequence(int[] count){
-        total++;
-        for(int pos =0; pos < 26; pos++){
-            if(count[pos] == 0){
-                continue;
-            }
-            count[pos] --;
-            findSequence(count);
-            count[pos]++;
-        }
-    }
+    int n;
     public int numTilePossibilities(String tiles) {
-        total = 0;
-        int[] count = new int[26];
-        for(char c : tiles.toCharArray()){
-            count[c - 'A']++;
+        n = tiles.length();
+        boolean[] used = new boolean[n];
+        Set<String> result = new HashSet<>();
+        StringBuilder curr = new StringBuilder();
+
+        solve(tiles,used,result,curr);
+        return result.size()-1;
+    }
+    public void solve(String tiles, boolean[] used, Set<String> result, StringBuilder curr){
+        result.add(curr.toString());
+        for(int i=0; i<n ;i ++){
+            if(used[i]) continue;
+            curr.append(tiles.charAt(i));
+            used[i] = true;
+            solve(tiles,used,result,curr);
+            used[i] = false;
+            curr.deleteCharAt(curr.length() -1 );
         }
-        findSequence(count);
-        return  total -1;
-        
     }
 }
+/*
+AAB
+A,B,AA,AB,BA,AAB,BAA,,ABA
+
+*/
