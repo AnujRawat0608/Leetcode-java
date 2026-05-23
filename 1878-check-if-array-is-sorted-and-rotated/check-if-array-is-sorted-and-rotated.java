027131b225ad71c8f2a1fcdaa5b15224c46dfb08
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-05-23 15:40:11 +0530

    Time: 4 ms (2.88%) | Memory: 43.3 MB (11.32%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,17 +1,42 @@
 class Solution {
     public boolean check(int[] nums) {
 
-        int count = 0;
         int n = nums.length;
+        //step 1: create sorted version
+        int[] sorted = nums.clone();
+        Arrays.sort(sorted);
 
-        for(int i = 0; i < n; i++) {
+        //step 2 : try all the rotations
+        for(int r = 0; r < n; r ++){
+            boolean match = true;
+            //compare rotated array with original nums
+            for(int i=0; i<n; i++){
+                int rotatedindex = (i + r) % n ;
 
-            if(nums[i] > nums[(i + 1) % n]) {
-                count++;
+                if(sorted[rotatedindex] != nums[i]){
+                    match = false;
+                    break;
+                }
             }
-
+            if(match){
+                return true;
+            }
         }
-
-        return count <= 1;
+        return false;
     }
 }
+
+/*
+
+3,4,5,1,2
+
+-fir sort the array
+1,2,3,4,5
+
+aab ek ek bar rotate
+5,1,2,3,4 -> fir match with input array if not same rotate
+4,5,1,2,3 ->fir se rotate
+3,4,5,1,2,-> it matches the input array
+
+
+*/
