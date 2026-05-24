8d60ecf6d8517296fa2b5ed7a7481ccbf353d2af
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-05-24 22:04:43 +0530

    Time: 22 ms (89.95%) | Memory: 77.7 MB (84.36%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,21 +1,19 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums.length == 0){
-            return 0;
-        }
-
-
         Arrays.sort(nums);
         int longest = 1;
         int currentstreak = 1;
 
-        for(int i=1; i<nums.length; i++){
+        if(nums.length == 0){
+            return 0;
+        }
 
+        for(int i=1; i<nums.length; i++){
             if(nums[i] == nums[i-1]){
                 continue;
             }
             if(nums[i] == nums[i-1]+1){
-                currentstreak++;
+                currentstreak ++;
             }else{
                 currentstreak = 1;
             }
