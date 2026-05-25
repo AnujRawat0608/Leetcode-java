a214e6c93635eb31c8d2817d4a176d31f6ca125f
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-05-25 10:09:53 +0530

    Time: 10 ms (87.60%) | Memory: 46.4 MB (58.13%) - LeetSync

--- 1466-jump-game-v/jump-game-v.java
+++ 1466-jump-game-v/jump-game-v.java
@@ -1,43 +1,30 @@
 class Solution {
     int n;
-    int[] t;
+    int[] dp;
 
-    private int solve(int[] arr, int d, int i) {
-        if (t[i] != -1)
-            return t[i];
-
-        int result = 1; // count current index also
-
-        // move left
-        for (int j = i - 1; j >= Math.max(0, i - d); j--) {
-            // can't jump further once taller/equal element appears
-            if (arr[j] >= arr[i])
-                break;
-            result = Math.max(result, 1 + solve(arr, d, j));
+    private int solve(int[]arr, int d, int i){
+        int result = 1;
+        if(dp[i] != 0){
+            return dp[i];
         }
-
-        // move right
-        for (int j = i + 1; j <= Math.min(n - 1, i + d); j++) {
-            // can't jump further once taller/equal element appears
-            if (arr[j] >= arr[i])
-                break;
-            result = Math.max(result, 1 + solve(arr, d, j));
+        for(int j=i-1; j>= Math.max(0,i-d);j--){
+            if(arr[j] >= arr[i])break;
+            result = Math.max(result,1+solve(arr,d,j));
         }
-
-        return t[i] = result;
+        for(int j=i+1;j<=Math.min(n-1,i+d); j++){
+            if(arr[j] >= arr[i])break;
+            result = Math.max(result,1+solve(arr,d,j));
+        }
+        dp[i] = result;
+        return dp[i];
     }
-
     public int maxJumps(int[] arr, int d) {
         n = arr.length;
-        t = new int[n];
-        Arrays.fill(t, -1);
-
+        dp = new int[n];
         int maxJump = 1;
-        // We need to try from every index
-        for (int i = 0; i < n; i++) {
-            maxJump = Math.max(maxJump, solve(arr, d, i));
-        }
-
+        for(int i=0; i<n; i++){
+            maxJump = Math.max(maxJump,solve(arr,d,i));
+        }   
         return maxJump;
     }
 }
