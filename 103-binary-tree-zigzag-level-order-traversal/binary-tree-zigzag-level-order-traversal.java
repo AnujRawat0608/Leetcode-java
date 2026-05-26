83738ae2fc918f20ae3f8ace435045ffb9a65e3b
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-05-26 14:43:11 +0530

    Time: 1 ms (78.62%) | Memory: 43.6 MB (78.69%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -15,34 +15,36 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> zigzag = new ArrayList<>();
-        if(root == null) return zigzag;
-
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null){
+            return result;
+        }
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
-        boolean flag = false;
 
+        boolean leftToRight = true;
         while(!queue.isEmpty()){
-
-            int size = queue.size();
             List<Integer> level = new ArrayList<>();
-            Stack<Integer> reverseStack = new Stack<>();
-            for(int i= 0; i<size;i++){
-                TreeNode node = queue.poll();
+            int size = queue.size();
 
-                if(flag)
-                reverseStack.add(node.val);
-                else
-                level.add(node.val);
+            for(int i=0; i<size; i++){
+                TreeNode curr = queue.poll();
+                level.add(curr.val);
 
-                if(node.left != null)queue.add(node.left);
-                if(node.right != null) queue.add(node.right);
+                if(curr.left != null){
+                    queue.add(curr.left);
+                }
+                if(curr.right != null){
+                    queue.add(curr.right);
+                }
             }
-            flag = !flag;
-            while(!reverseStack.isEmpty())
-            level.add(reverseStack.pop());
-            zigzag.add(level);
+            if(!leftToRight){
+                Collections.reverse(level);
+            }
+            result.add(level);
+            leftToRight = !leftToRight;
         }
-        return zigzag;
+        return result;
+        
     }
 }
