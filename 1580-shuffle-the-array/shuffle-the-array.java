4e2655b22d8b054ab4f99e7154b0a671e68d8857
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-05-26 10:37:18 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (79.58%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] result = new int[2*n];
+        int idx = 0; 
+        for(int i=0; i<n; i++){
+            result[idx] = nums[i];
+            idx++;
+            result[idx] = nums[i+n];
+            idx++;
+        }
+        return result;
+        
+    }
+}
