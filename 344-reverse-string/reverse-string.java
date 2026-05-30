e8f2ef579ef7b1249b431ac3cabab5a6d9dd4983
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-05-30 16:48:51 +0530

    Time: 1 ms (21.64%) | Memory: 47.6 MB (99.39%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -4,13 +4,13 @@ class Solution {
         char[] result = new char[n];
         int idx = 0;
 
-        for(int i=n-1; i>=0; i--){
+        for(int i= n-1; i>=0; i--){
             result[idx] = s[i];
             idx++;
         }
         for(int i=0; i<n; i++){
-            s[i] = result[i]; 
+            s[i] = result[i];
         }
-        
+         
     }
 }
