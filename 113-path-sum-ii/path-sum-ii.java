4b19b79406118438f0adf5b2fcb4e46372c8ba00
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-06-01 21:05:07 +0530

    Time: 1 ms (100.00%) | Memory: 45.2 MB (85.16%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> currPath = new ArrayList<>();
+        findPaths(root,targetSum,0,currPath,result);
+        return result;
+    }
+    private void findPaths(TreeNode node, int targetSum, int currentSum, List<Integer>currPath,List<List<Integer>> result){
+        if(node == null){
+            return;
+        }
+        currPath.add(node.val);
+        currentSum += node.val;
+
+        if(node.left == null && node.right == null){
+            if(currentSum == targetSum){
+                result.add(new ArrayList<>(currPath));
+            }
+            currPath.remove(currPath.size() -1);
+            return;
+        }
+        findPaths(node.left,targetSum,currentSum,currPath,result);
+        findPaths(node.right,targetSum,currentSum,currPath,result);
+
+        currPath.remove(currPath.size() -1);
+    }
+}
