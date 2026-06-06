1c9820878c7e786ed6372e7e6219d941bcd20ab5
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-06-07 01:03:09 +0530

    Time: 3 ms (7.80%) | Memory: 46 MB (10.16%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,42 +1,36 @@
 class Solution {
 
     List<List<Integer>> result = new ArrayList<>();
+    Set<Integer> used = new HashSet<>();
     int n;
 
     public List<List<Integer>> permute(int[] nums) {
         n = nums.length;
 
-        solve(0, nums);
+        backtrack(new ArrayList<>(), nums);
 
         return result;
     }
 
-    private void solve(int idx, int[] nums) {
+    private void backtrack(List<Integer> temp, int[] nums) {
 
-        if (idx == n) {
-            List<Integer> temp = new ArrayList<>();
-
-            for (int num : nums) {
-                temp.add(num);
-            }
-
-            result.add(temp);
+        if (temp.size() == n) {
+            result.add(new ArrayList<>(temp));
             return;
         }
 
-        for (int i = idx; i < n; i++) {
+        for (int i = 0; i < n; i++) {
 
-            swap(nums, i, idx);
+            if (!used.contains(nums[i])) {
 
-            solve(idx + 1, nums);
+                temp.add(nums[i]);
+                used.add(nums[i]);
 
-            swap(nums, i, idx); // backtrack
+                backtrack(temp, nums);
+
+                used.remove(nums[i]);
+                temp.remove(temp.size() - 1);
+            }
         }
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
     }
 }
