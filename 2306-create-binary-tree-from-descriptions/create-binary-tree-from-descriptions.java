78ec476656121d47a1c479af86cd07b050633c9e
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-06-07 12:15:00 +0530

    Time: 68 ms (51.16%) | Memory: 86.1 MB (54.34%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
@@ -15,35 +15,31 @@
  */
 class Solution {
     public TreeNode createBinaryTree(int[][] descriptions) {
-        Map<Integer, TreeNode> mp = new HashMap<>();
+        Map<Integer,TreeNode> mp = new HashMap<>();
         Set<Integer> childSet = new HashSet<>();
 
-        // Create nodes and set up parent-child relationships
-        for (int[] description : descriptions) {
+        for(int[] description : descriptions){
             int parent = description[0];
             int child = description[1];
-            boolean isLeft = description[2] == 1;
+            boolean isLeft = description[2] ==1;
 
-            mp.putIfAbsent(parent, new TreeNode(parent));
-            mp.putIfAbsent(child, new TreeNode(child));
+            mp.putIfAbsent(parent,new TreeNode(parent));
+            mp.putIfAbsent(child,new TreeNode(child));
 
-            if (isLeft) {
+            if(isLeft){
                 mp.get(parent).left = mp.get(child);
-            } else {
+            }else{
                 mp.get(parent).right = mp.get(child);
-            }
-
-            childSet.add(child);
+             }
+             childSet.add(child);
         }
-
-        // Find the root node (which is not any child)
-        for (int[] description : descriptions) {
+        for(int[] description : descriptions){
             int parent = description[0];
-            if (!childSet.contains(parent)) {
+            if(!childSet.contains(parent)){
                 return mp.get(parent);
             }
         }
-
         return null;
+
     }
 }
