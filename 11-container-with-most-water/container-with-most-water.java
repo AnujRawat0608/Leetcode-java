480884d17025c3dadca937951f2e3ef87a6114c1
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-06-14 14:07:30 +0530

    Time: 5 ms (82.78%) | Memory: 77.4 MB (28.15%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,24 +1,54 @@
 class Solution {
     public int maxArea(int[] height) {
+
         int n = height.length;
-        int left = 0; 
-        int right = n - 1;
-        int maxAre = 0;
+        int left = 0;
+        int right = n-1;
+         int maxArea = 0;
 
         while(left < right){
-            int h = Math.min(height[left], height[right]);
-            int w = right - left;
-            int currArea = h * w;
+           
+            int length = Math.min(height[left],height[right]);
+            int breadth = right - left;
+            int area = length * breadth;
 
-            maxAre = Math.max(maxAre,currArea);
-
-            if(height[left] < height[right]){
-                left ++;
+            maxArea = Math.max(maxArea,area);
+            if(height[left]<height[right]){
+            left++;
             }else{
-                right --;
+                right--;
+            
             }
         }
-        return maxAre;
+        
+
+        return maxArea;
         
     }
 }
+
+/*
+
+int[] height
+n vertical lines 
+
+return maximum amount of water a container can store 
+int n = height.length;
+i =0;
+j=n-1;
+
+height =[]
+u have to remove the area  of the container to find out the maximum amount of water the container can hold
+area = length * breadth(width)
+
+length = minimum length between 2 verical lines will be the length 
+int n = nums.length ;
+breadth =int i = 0
+int j = n-1;
+
+
+while(left < right){
+
+}
+
+*/
