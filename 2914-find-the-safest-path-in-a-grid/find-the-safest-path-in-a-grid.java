750d59789723a47a05f3caddb1654d20cab41395
AnujRawat0608 <126395510+AnujRawat0608@users.noreply.github.com>
2026-07-01 17:37:55 +0530

    Time: 251 ms (11.43%) | Memory: 126.8 MB (19.05%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.java
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.java
@@ -1,80 +1,102 @@
 class Solution {
-    public int maximumSafenessFactor(List<List<Integer>> grid) {
-        int n = grid.size();
-        if (grid.get(0).get(0) == 1 || grid.get(n - 1).get(n - 1) == 1 || n == 1)
-            return 0;
-            
-        int arr[][] = new int[n][n];
+    int n;
+    int[][] directions = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                arr[i][j] = grid.get(i).get(j);
+    boolean check(int[][] distNearestThief, int sf) {
+        Queue<int[]> que = new LinkedList<>();
+        boolean[][] visited = new boolean[n][n];
+
+        que.add(new int[]{0, 0});
+        visited[0][0] = true;
+
+        if(distNearestThief[0][0] < sf)
+            return false;
+
+        while(!que.isEmpty()) {
+            int[] curr = que.poll();
+            int curr_i = curr[0];
+            int curr_j = curr[1];
+
+            if(curr_i == n-1 && curr_j == n-1) {
+                return true;
             }
-        }
-        arr = updateMatrix(arr);
-        
-        int low = 0, high = (n - 1) * 2;
-        int mid = 0; 
-        int result = mid;
-        while(low <= high) {
-            mid = (high - low) / 2 + low;
-            if(dfs(arr,0, 0, mid, n, new boolean[n][n])) {
-                result = mid;
-                low = mid + 1;
-            }
-            else high = mid - 1;
-        }
-        return result;
-    }
 
-    public int[][] updateMatrix(int[][] matrix) {
-        int n = matrix.length;
+            for(int[] dir : directions) {
+                int new_i = curr_i + dir[0];
+                int new_j = curr_j + dir[1];
 
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < n; col++) {
-                matrix[row][col] ^= 1;
-                if (matrix[row][col] == 1) {
-                    matrix[row][col] = Integer.MAX_VALUE / 2;
+                if(new_i >= 0 && new_i < n && new_j >= 0 && new_j < n && !visited[new_i][new_j]) {
+                    if(distNearestThief[new_i][new_j] < sf) {
+                        continue; //reject this cell
+                    }
+                    que.add(new int[]{new_i, new_j});
+                    visited[new_i][new_j] = true;
                 }
             }
         }
 
-        for (int row = 0; row < n; row++) {
-            for (int col = 1; col < n; col++) {
-                matrix[row][col] = Math.min(matrix[row][col], matrix[row][col - 1] + 1);
-            }
-            for (int col = n - 2; col >= 0; col--) {
-                matrix[row][col] = Math.min(matrix[row][col], matrix[row][col + 1] + 1);
-            }
-        }
-
-        for (int col = 0; col < n; col++) {
-            for (int row = 1; row < n; row++) {
-                matrix[row][col] = Math.min(matrix[row][col], matrix[row - 1][col] + 1);
-            }
-            for (int row = n - 2; row >= 0; row--) {
-                matrix[row][col] = Math.min(matrix[row][col], matrix[row + 1][col] + 1);
-            }
-        }
-
-        return matrix;
-    }
-    
-    int[] dx = {-1, 1, 0, 0};
-    int[] dy = {0, 0, 1, -1};
-    boolean dfs(int arr[][], int r, int c, int mid, int n, boolean[][] visited) {
-        if(visited[r][c] || arr[r][c] < mid) return false;
-        if(r == n - 1 && c == n - 1) return true;
-
-        visited[r][c] = true;
-        for(int i = 0; i < 4; i++) {
-            int x = r + dx[i];
-            int y = c + dy[i];
-
-            if(x == -1 || y == -1 || x == n || y == n) continue;
-            if(dfs(arr, x, y, mid, n, visited)) return true;
-        }
         return false;
     }
 
+    int maximumSafenessFactor(List<List<Integer>> grid) {
+        n = grid.size();
+
+        // Step-1 Precalculation of distNearestThief - for each cell
+        int[][] distNearestThief = new int[n][n];
+        Queue<int[]> que = new LinkedList<>();
+        boolean[][] visited = new boolean[n][n];
+
+        // Push all cells in queue where thieves are present
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid.get(i).get(j) == 1) {
+                    que.add(new int[]{i, j});
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        int level = 0;
+        while(!que.isEmpty()) {
+            int size = que.size();
+            while(size-- > 0) {
+                int[] curr = que.poll();
+                int curr_i = curr[0];
+                int curr_j = curr[1];
+
+                distNearestThief[curr_i][curr_j] = level;
+
+                for(int[] dir : directions) {
+                    int new_i = curr_i + dir[0];
+                    int new_j = curr_j + dir[1];
+
+                    if(new_i < 0 || new_i >= n || new_j < 0 || new_j >= n || visited[new_i][new_j]) {
+                        continue;
+                    }
+
+                    que.add(new int[]{new_i, new_j});
+                    visited[new_i][new_j] = true;
+                }
+            }
+            level++;
+        }
+
+        // Step-2 Apply binary search on SF
+        int l = 0;
+        int r = 400;
+        int result = 0;
+
+        while(l <= r) {
+            int mid_sf = l + (r - l) / 2;
+
+            if(check(distNearestThief, mid_sf)) {
+                result = mid_sf;
+                l = mid_sf + 1;
+            } else {
+                r = mid_sf - 1;
+            }
+        }
+
+        return result;
+    }
 }
